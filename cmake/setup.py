3761a7e2ca127161d0da7a53fa5c2557ce5d8ac9
cielavenir <cielartisan@gmail.com>
2022-03-20 01:41:10 +0900

    bump version

--- cmake/setup.py
+++ cmake/setup.py
@@ -126,7 +126,7 @@ setup(
     name='codecs7z',
     description='a (light) binding for 7-zip codecs',
     long_description=open("../README.md").read(),
-    version='0.0.0.5',
+    version='0.0.0.6',
     url='https://github.com/cielavenir/python-codecs7z',
     license='LGPL',
     author='cielavenir',
